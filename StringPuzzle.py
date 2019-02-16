30c60036e45a721830fa861d788413e5cacd0965
PRABHU SUNDARRAJ <43057160+prabhus489@users.noreply.github.com>
2019-02-17 00:06:18 +0530

    Interview Question
    String(Immutable & muttable) 
    Global & Local Variable 
    puzzle

---
+++ StringPuzzle.py
@@ -0,0 +1,17 @@
+
+def cocat(String, array):
+    String = String + "Nadu"
+    array.append("b")
+    print (String)
+    print (array)
+    return String
+
+String = "Tamil"
+
+array =  ["a"]
+
+cocat(String, array)
+
+print (String)
+print (array)
+
