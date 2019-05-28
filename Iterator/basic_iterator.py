c22c8f0d386bf9c608710ee7353b47a9faa2e981
Prabhu Sundarraj <prabhu12yuva@gmail.com>
2019-05-28 13:38:20 +0530

    Update basic_iterator.py

--- Iterator/basic_iterator.py
+++ Iterator/basic_iterator.py
@@ -1,17 +1,17 @@
 print(f"------Iterating Over list------")
-#Iterating over  list
+# Iterating over  list
 sp_list = [1, 2, 3, 4, 5]
 for i in sp_list:
     print(i)
 
 print(f"------Iterating Over String-------")
-#Iterating Over String
+# Iterating Over String
 sp_string = "Generator"
 for s in sp_string:
     print(s)
 
 print(f"------Iterating Over Dictionary------")
-#Iterating Over  Dictionary
+# Iterating Over  Dictionary
 sp_dict = {"Name":"Prabhu", "PH No":7401256086, "EMP_ID":1323}
 for key,value in sp_dict.items():
     print(key,value)
