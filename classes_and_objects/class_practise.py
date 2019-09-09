32766fe7b73d356779903c3d2a27883b9c6be964
PRABHU SUNDARRAJ <43057160+prabhus489@users.noreply.github.com>
2019-09-09 17:22:55 +0530

    Class and object practice
    python class and object

---
+++ classes_and_objects/class_practise.py
@@ -0,0 +1,5 @@
+class User:
+    pass
+
+user1 = User
+print(user1)
---
+++ classes_and_objects/class_test1.py
@@ -0,0 +1,13 @@
+class MyStuff(object):
+    def __init__(self):
+        self.tangerine = "Hi I am Prabhu init method"
+
+    def apple(self):
+        print("I am classy apple")
+
+
+
+
+thing = MyStuff()
+thing.apple()
+print(thing.tangerine)
